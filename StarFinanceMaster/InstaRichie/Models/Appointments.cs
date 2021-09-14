c584e297413ce9cc1b04dbccd829c2a6c8ef5b35
steph <stephanieq7@yahoo.com.au>
2021-09-14 17:53:57 +0930

    Basic c# code - still needs bugs fixed

---
+++ StarFinanceMaster/InstaRichie/Models/Appointments.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+namespace StartFinance.Models
+{
+    class Appointments
+    {
+        [PrimaryKey, AutoIncrement]
+        public int AppointmentID { get; set; }
+
+        [NotNull]
+        public string EventName { get; set; }
+
+        [NotNull]
+        public string Location { get; set; }
+
+        [NotNull]
+        public DateTime EventDate { get; set; }
+
+        [NotNull]
+        public DateTime StartTime { get; set; }
+
+        [NotNull]
+        public DateTime EndTime { get; set; }
+    }
+}
--- StarFinanceMaster/InstaRichie/Views/AppointmentsPage.xaml.cs
+++ StarFinanceMaster/InstaRichie/Views/AppointmentsPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+using StartFinance.Models;
+using SQLite.Net;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +25,128 @@ namespace StartFinance.Views
     /// </summary>
     public sealed partial class AppointmentsPage : Page
     {
+        SQLiteConnection conn; // adding an SQLite connection
+        string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Findata.sqlite");
+
         public AppointmentsPage()
         {
             this.InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+            /// Initializing a database
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+
+            // Creating table
+            Results();
         }
+        public void Results()
+        {
+            // Creating table
+            conn.CreateTable<Appointment>();
+            var query = conn.Table<Appointment>();
+            AppointmentList.ItemsSource = query.ToList();
+        }
+
+        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // checks if account name is null
+                if (EventNameBox.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("Amount Name not Entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else if (EventNameBox.Text.ToString() == "AccountName" || EventNameBox.Text.ToString() == "InitialAmount")
+                {
+                    MessageDialog variableerror = new MessageDialog("You cannot use this name", "Oops..!");
+                }
+                else
+                {   // Inserts the data
+                    conn.Insert(new Appointment()
+                    {
+                        EventName = EventNameBox.Text,
+                        Location = LocationBox.Text,
+                        EventDate = appointmentDatePicker.Date,
+                        StartTime = appStartTimePicker.Time,
+                        EndTime = appEndTimePicker.Time,
+                    });
+                    Results();
+                }
+
+            }
+            catch (Exception ex)
+            {   // Exception to display when amount is invalid or not numbers
+                if (ex is FormatException)
+                {
+                    MessageDialog dialog = new MessageDialog("You forgot to enter the Amount or entered an invalid data", "Oops..!");
+                    await dialog.ShowAsync();
+                }   // Exception handling when SQLite contraints are violated
+                else if (ex is SQLiteException)
+                {
+                    MessageDialog dialog = new MessageDialog("Account Name already exist, Try Different Name", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    /// no idea
+                }
+
+            }
+        }
+
+        // Clears the fields
+        private async void ClearFileds_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog ClearDialog = new MessageDialog("Cleared", "information");
+            await ClearDialog.ShowAsync();
+        }
+
+        // Displays the data when navigation between pages
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Results();
+        }
+
+        private async void DeleteItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog ShowConf = new MessageDialog("Deleting this Account will delete all transactions of this account", "Important");
+            ShowConf.Commands.Add(new UICommand("Yes, Delete")
+            {
+                Id = 0
+            });
+            ShowConf.Commands.Add(new UICommand("Cancel")
+            {
+                Id = 1
+            });
+            ShowConf.DefaultCommandIndex = 0;
+            ShowConf.CancelCommandIndex = 1;
+
+            // sort out this bit vvv
+
+            //var result = await ShowConf.ShowAsync();
+            //if ((int)result.Id == 0)
+            //{
+            //    // checks if data is null else inserts
+            //    try
+            //    {
+            //        string AppointmentsLabel = ((Appointments)AppointmentList.SelectedItem).EventName;
+            //        var querydel = conn.Query<Appointments>("DELETE FROM Appointments WHERE EventName='" + AppointmentsLabel + "'");
+            //        Results();
+            //        conn.CreateTable<Appointments>();
+            //        var querytable = conn.Query<Appointments>("DELETE FROM Appointments WHERE Appointments='" + AppointmentsLabel + "'");
+            //    }
+            //    catch (NullReferenceException)
+            //    {
+            //        MessageDialog ClearDialog = new MessageDialog("Please select the item to Delete", "Oops..!");
+            //        await ClearDialog.ShowAsync();
+            //    }
+            //}
+            //else
+            //{
+            //    //
+            //}
+        }
+
+
     }
 }
